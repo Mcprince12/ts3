1e384a2b11b569b4eba2d940a2d7dd486f7213bc
Mcprince12 <68846631+Mcprince12@users.noreply.github.com>
2020-08-30 10:34:10 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var stand;
+var score = 0;
+
+function setup() {
+  createCanvas(800,400);
+  
+  text("SCORE : "+score, 750, 40);
+  stand = new Ground(700, 10, 100, 300);
+
+  block1 = new Box(330, 235, 30, 40);
+  block2 = new Box(360, 235, 30, 40);
+  block3 = new Box(390, 235, 30, 40);
+  block4 = new Box(420, 235, 30, 40);
+  block5 = new Box(450, 235, 30, 40);
+
+  block6 = new Box(360, 195, 30, 40);
+  block7 = new Box(390, 195, 30, 40);
+  block8 = new Box(420, 195, 30, 40);
+
+  polygon = Bodies.circle(50, 200, 20);
+  World.add(world, polygon);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  stand.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  
+  
+  drawSprites();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+  
+}
+
+function mouseReleased(){
+  slingshot.fly();
+  
+}
